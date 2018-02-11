18290b23d79242be1fe5c032534191d33e7b001f
Nicholas Jacquet <nicholasjacquet2@gmail.com>
2018-02-11 01:26:01 -0600

    Update Mandelbrot.java

--- Mandelbrot.java
+++ Mandelbrot.java
@@ -6,122 +6,20 @@ import java.awt.*;
 import java.awt.image.*;
 import java.awt.event.*;
 import javax.swing.*;
+import javax.swing.event.MouseInputAdapter;
 
 /**
-  * @author Nicholas Jacquet
-  * Notes:
+  * 
+  * @author Nicholas
   *
-  *     The Mandelbrot Set is the visual representation 
-  *     of an iterated function on the complex plane.
-  * 
-  *     ~Simple function example~
-  * 
-  *     INPUT -> FUNCTION -> OUTPUT
-  *                f(x)
-  *             f(INPUT) = OUTPUT       
-  *          
-  *     ~Iterated function example~
-  *     
-  * [Iterated] f(x) = x + 1
-  *         f(2) = 2 + 1 = 3;   // from here on down the 
-  *         f(3) = 3 + 1 = 4;   // OUTPUT of the function      
-  *         f(4) = 4 + 1 = 5;   // is then used as INPUT
-  *         f(5) = 5 + 1 = 6;   // of the next FUNCTION  
-  *     
-  * [Iterated] f(x) = x^2 + c             [say c is 2]
-  *         f(   2) =       4 + 2 = 6
-  *         f(   6) =      36 + 2 = 38
-  *         f(  38) =    1444 + 2 = 1446
-  *         f(1448) = 2090916 + 2 = 2090918
-  *                  [OUTPUT DIVERGENCE]
+  *	Notes:
   *
-  * [Iterated] f(x) = x^2 + c             [say c is 0.1]
-  *         f(      0.1) =               0.01 + 0.1 = 0.11
-  *         f(     0.11) =             0.0121 + 0.1 = 0.1121
-  *         f(   0.1121) =         0.01256641 + 0.1 = 0.11256641
-  *         f(0.1256641) = 0.0126711966602881 + 0.1 = 0.112671196660288   
-  *                  [OUTPUT CONVERGENCE]
-  * 
-  * 
-  * The idea is that numbers between -2 and 0 behave differently!
-  * 
-  *     Negative * Negative = Positive
-  *     (example:)
-  *     
-  *     -2 * -3 = 6
-  *     -5 ^  2 = 25
-  *     
-  *     Adding a Negative = Subtracting
-  *     (example:) 
-  *     5 + -6 = 5 - 6
-  * 
-  * 
-  * [Iterated] f(x) = x^2 + c             [say c is -1.5]
-  *     f(       -1.5) = 2.25              - 1.5 =  0.75
-  *     f(       0.75) = 0.5625            - 1.5 = -0.9375
-  *     f(    -0.9375) = 0.87890625        - 1.5 = -0.62109375
-  *     f(-0.62109375) = 0.385757446289063 - 1.5 = -1.11424255371094
-  * 
-  * References:
-  * 
-  *     ~The Amazing Mandelbrot Set~
-  *     https://www.youtube.com/watch?v=0YaYmyfy9Z4
-  * 
-  *     -2 & -1 both behave very strangely when iterated thru:
-  *     
-  *     [say c is -2] ~the outputs when iterating will forever be 2
-  *     [say c is -1] ~the outputs when iterating will flip flop back and forth as  0 & -1
-  *     
-  *     
-  *     
-  *     
-  * ~Complex Numbers also cause strange things to happen when iterated thru:    
-  *     
-  *     All negative numbers have positive squares:
-  *         
-  *             2^2 = 4  &&  -2^2 = 4
-  *        
-  *     (-2 & 2 are both squareroots of 4)
-  *     
-  *     
-  *     What is the squareroot of -4? (hint: The squareroot of any negative number is an imaginary number)
-  * 
-  * 
-  * ~IMAGINARY NUMBER is the square root of a negative number
-  * ~COMPLEX   NUMBER is the sum of an IMAGINARY NUMBER and REAL NUMBER
-  *     
-  *   examples: 
-  *   
-  *     (Math.Sqrt(-4)    )  ~equals a IMAGINARY NUMBER
-  *     (Math.Sqrt(-4) + 3)  ~equals a COMPLEX   NUMBER
-  *     
-  *     
-  * So what can I do with this?    
-  *     
-  *               -4  = 4 * -1
-  *     Math.Sqrt(-4) = Math.Sqrt(4 * -1)
-  *     Math.Sqrt(-4) = Math.Sqrt(4) * Math.Sqrt(-1)
-  *     Math.Sqrt(-4) = 2 * Math.Sqrt(-1)
-  *     
-  *     
-  *=====================================================================
-  *     
-  *     COMPLEX  = (REAL + IMAGINARY)   
-  *  
-  *       example: (3 + Math.Sqrt(-4))
-  * standard form: (3 + (2*(Math.Sqrt(-1)))
-  *            or: (3 + 2i)                      [i = Math.Sqrt(-1)]
-  *            
-  *       ~i is an IMAGINARY NUMBER     
-  *       
-  *=====================================================================       
-  *       
-  *      General form of a complex number: 
-  *       
-  *                 A + Bi
-  *     (example:)  3 + 2i         
-  *                 
-  *            
+  *		What events are there within this program?
+  *
+  *		java.awt.image
+  *			-
+  *
+  *
   **/
 
 public class MandelBrot extends JFrame
@@ -132,28 +30,14 @@ public class MandelBrot extends JFrame
 	
 	Canvas canvas;
 	BufferedImage fractalImage;
-	
-	//static final int MAX_ITER = 200;
 	static final int MAX_ITER = 600;
-	
-	static final double DEFAULT_ZOOM       = 300.0;
-	static final double DEFAULT_TOP_LEFT_X = -2.0;
-	static final double DEFAULT_TOP_LEFT_Y = 1.5;
-	
-	
+	static final double DEFAULT_ZOOM       =  500.0;
+	static final double DEFAULT_TOP_LEFT_X =  -2.25;                    // -2.0
+	static final double DEFAULT_TOP_LEFT_Y =  1.1;                        // 1.5
 	double zoomFactor = DEFAULT_ZOOM;
 	double topLeftX   = DEFAULT_TOP_LEFT_X;
 	double topLeftY   = DEFAULT_TOP_LEFT_Y;
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
+
 	// entry point
 	public static void main(String[] args) 
 	{
@@ -165,43 +49,12 @@ public class MandelBrot extends JFrame
 	// default Contructor
 	public MandelBrot() 
 	{
-		setInitialGUIProperties();
-		addCanvas();
-		canvas.addKeyStrokeEvents();
-		
-		//canvas.
-		
-		updateFractal();
-		this.setVisible(true);
+		doIt();
 	}
 	// default Contructor	
-	
-	
-	
-	/***
-	   *  
-	   ***/
-	
-	
-	
+
 	final Frame f = new Frame("DeviceIdentificationTest");
-	
-   		
 
-    			
-
-    /* ActionListener */
-
- 
-	
-
-	/***
-	    * 
-	    ***/
-	
-	
-	
-	
 	private void addCanvas() 
 	{
 		canvas = new Canvas();
@@ -209,25 +62,21 @@ public class MandelBrot extends JFrame
 		canvas.setVisible(true);
 		this.add(canvas, BorderLayout.CENTER);
 	}
-	
-	
+
 	private void setInitialGUIProperties() 
 	{
 		this.setTitle("Fractal Explorer");
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		this.setSize(WIDTH, HEIGHT);
-		//this.setSize(900, 900);
 		this.setResizable(false);
 		this.setLocationRelativeTo(null);
 	}
-	
-	
+
 	private double getXPos(double x) 
 	{
 		return x/zoomFactor + topLeftX;
 	}
-	
-	
+
 	private double getYPos(double y) 
 	{
 		return y/zoomFactor - topLeftY;
@@ -250,10 +99,7 @@ public class MandelBrot extends JFrame
 		}
 		canvas.repaint();
 	}
-	
-	
-	
-	
+
 	/** Returns a posterized color based off of the iteration count
     of a given point in the fractal **/
 	private int makeColor( int iterCount ) 
@@ -261,7 +107,7 @@ public class MandelBrot extends JFrame
 		int color = 0b011011100001100101101000; 
 		int mask  = 0b000000000000010101110111; 
 		int shiftMag = iterCount / 13;
-	
+
 		if (iterCount == MAX_ITER) 
 		{	
 			return Color.BLACK.getRGB();
@@ -273,54 +119,38 @@ public class MandelBrot extends JFrame
 
 	private int computeIterations(double c_r, double c_i) 
 	{
-		
 		/*
-		
 		Let c = c_r + c_i
 		Let z = z_r + z_i
-		
 		z' = z*z + c
 		   = (z_r + z_i)(z_r + z_i) + (c_r + c_i)
 		   = z_r² + 2*z_r*z_i - z_i² + c_r + c_i
 
 		     z_r' = z_r² - z_i² + c_r
 		     z_i' = 2*z_i*z_r + c_i
-		     
 		*/
-
 		double z_r = 0.0;
 		double z_i = 0.0;
-		
 		int iterCount = 0;
-
 		// Modulus (distance) formula:
 		// sqrt(a² + b²) <= 2.0
 		// a² + b² <= 4.0
 		while ( z_r*z_r + z_i*z_i <= 4.0 ) 
 		{
 			double z_r_tmp = z_r;
-			
 			z_r = z_r*z_r - z_i*z_i + c_r;
 			z_i = 2*z_i*z_r_tmp + c_i;
-			
 			// Point was inside the Mandelbrot set
 			if (iterCount >= MAX_ITER) 
 			{ 
 				return MAX_ITER;
 			}
-			
 			iterCount++;
-			
 		}
-		
 		// Complex point was outside Mandelbrot set
 		return iterCount;
-		
 	}
-	
-	
-	
-	
+
 	private void moveUp() 
 	{
 		double curHeight = HEIGHT / zoomFactor;
@@ -335,50 +165,52 @@ public class MandelBrot extends JFrame
 		updateFractal();
 	}
 	
-	
-	
 	private void moveLeft() 
 	{
 		double curWidth = WIDTH / zoomFactor;
 		topLeftX -= curWidth / 6;
 		updateFractal();
 	} 
-	
-	
-	
+
 	private void moveRight() 
 	{
 		double curWidth = WIDTH / zoomFactor;
 		topLeftX += curWidth / 6;
 		updateFractal();
 	}
-	
-	
-	
+
 	private void doIt() 
 	{
 		GraphicsConfiguration gc = f.getGraphicsConfiguration();
 		final GraphicsDevice gd = gc.getDevice();
 		
-		//System.out.println("--- Creating FS Frame on Device ---");
-		//System.out.println("Device  = " + gd);
-		//System.out.println(" bounds = " + gd.getDefaultConfiguration().getBounds());
-
 		/* AWT fullscreen */
 		final Frame fsf = new Frame("FullScreen" + gd, gc) 
 		{
-			
-			//addKeyListener();
-			
+			public void updateFractalAWT(Graphics g, int w, int h, BufferedImage bi) 
+			{
+				for (int x = 0; x < this.WIDTH ; x++ ) 
+				{
+					for (int y = 0; y < this.HEIGHT ; y++ ) 
+					{
+						double c_r = getXPos(x);
+						double c_i = getYPos(y);
+						
+						int iterCount = computeIterations(c_r, c_i);
+						int pixelColor = makeColor(iterCount);
+						
+						bi.setRGB(x, y, pixelColor);
+					}
+				}
+				g.drawImage(bi , 0, 0, Color.black, null );
+			}
+
 			public void paint(Graphics g)
 			{
 				g.setColor(Color.black);
 				g.fillRect(0, 0, getWidth(), getHeight());
 				g.setColor(Color.gray );
-				
-				//fractalImage = new BufferedImage(WIDTH, HEIGHT, BufferedImage.TYPE_INT_RGB);
 				fractalImage = new BufferedImage(getWidth(), getHeight(), BufferedImage.TYPE_INT_RGB);
-				
 				for (int x = 0; x < getWidth(); x++ ) 
 				{
 					for (int y = 0; y < getHeight(); y++ ) 
@@ -392,41 +224,27 @@ public class MandelBrot extends JFrame
 						fractalImage.setRGB(x, y, pixelColor);
 					}
 				}
-				
-				
-				//g.drawImage(img, x, y, bgcolor, observer)
-				g.drawImage(fractalImage , 200, 200, Color.black, null );
-				//updateFractal();
-				
-				//g.draw3DRect(425, 425,50, 40, true);
-				
-				//Font myFont = new Font("Serif", Font.BOLD, 24);
-				
-				//g.setFont(myFont);
-				//g.drawString("FS on device: " + gd, 200, 200);
-				//g.drawString("Click to exit Full-screen.", 200, 250);
-				
+			
+				g.drawImage(fractalImage , 0, 0, Color.black, null );
 				int i = 1;
-				
-				int yLoc = 300;
-				
-				while(i < 10) 
+
+				while(true)
 				{
-					yLoc = yLoc + 25;
-					
-					//g.drawString("Lorum ipsum quia dolor sit amet, consectetur, adipisci velit: " + i, 200, yLoc);
-					
-					//try 
-					//{
-						//TimeUnit.SECONDS.sleep(1);
-					//} 
-					//catch (InterruptedException e) 
-					//{
-						//g.drawString(e.getMessage().toString(), 200, yLoc);
-					//}
+					try 
+					{
+						TimeUnit.SECONDS.sleep(1);
+						//zoomFactor = zoomFactor + 50;
+						
+						//g.drawImage(fractalImage , 0, 0, Color.black, null );
+						//updateFractalAWT(getGraphics(), getWidth(), getHeight(), fractalImage );
+					} 
+					catch (InterruptedException e) 
+					{
+						
+					}
 					++i;
 				}
-				//g.drawString("Input Command: ", 200, yLoc+25);
+				
 			}
 		};
 
@@ -434,23 +252,63 @@ public class MandelBrot extends JFrame
 
 		fsf.addMouseListener(
 		
-				new MouseAdapter() 
+				//new MouseAdapter() 
+				new MouseInputAdapter()
 				{
+					
+					public void updateFractalAWT2(Graphics g, int w, int h, BufferedImage bi) 
+					{
+						for (int x = 0; x < bi.getWidth() ; x++ ) 
+						{
+							for (int y = 0; y < bi.getHeight() ; y++ ) 
+							{
+								double c_r = getXPos(x);
+								double c_i = getYPos(y);
+								
+								int iterCount = computeIterations(c_r, c_i);
+								int pixelColor = makeColor(iterCount);
+								
+								bi.setRGB(x, y, pixelColor);
+							}
+						}
+						g.drawImage(bi , 0, 0, Color.black, null );
+					}
+
+					private void adjustZoom( double newX, double newY, double newZoomFactor, BufferedImage bi ) 
+					{
+						topLeftX += newX/zoomFactor;
+						topLeftY -= newY/zoomFactor;
+						zoomFactor = newZoomFactor;
+						topLeftX -= ( WIDTH/2) / zoomFactor;
+						topLeftY += (HEIGHT/2) / zoomFactor;
+						updateFractalAWT2(getGraphics(), bi.getWidth(), bi.getHeight(), bi );
+					}
+
 					public void mouseClicked(MouseEvent e) 
 					{
 						gd.setFullScreenWindow(null);
-						fsf.dispose();
+						//fsf.dispose();
+						double x = (double) e.getX();
+						double y = (double) e.getY();
+						
+						switch( e.getButton() ) 
+						{
+							// Left
+							case MouseEvent.BUTTON1:
+								adjustZoom( x, y, zoomFactor*2, fractalImage  );
+								break;
+							
+							// Right
+							case MouseEvent.BUTTON3:
+								adjustZoom( x, y, zoomFactor/2, fractalImage  );
+								break;
+						}
 					}
 				}
 			);
-		
 		gd.setFullScreenWindow(fsf);
-		
 	}
-	
-	
-	
-	
+
 	// MouseListener
 	private class Canvas extends JPanel implements MouseListener 
 	{
@@ -464,10 +322,7 @@ public class MandelBrot extends JFrame
 			updateFractal();
 		}
 		// MouseListener		
-		
-		
-		
-		
+
 		public Canvas() 
 		{
 			addMouseListener(this);
@@ -579,6 +434,4 @@ public class MandelBrot extends JFrame
 		@Override public void mouseExited (MouseEvent mouse) { }
 		
 	} // Canvas
-
-
 }
